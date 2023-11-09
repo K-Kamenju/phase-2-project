a7a43d9efe8a523bab33f0d293c1781aa9c8fc45
Caleb <kamenjuhkiiru@gmail.com>
2023-11-09 11:46:11 +0300

    Search Bar changed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -6,7 +6,7 @@ function SearchBar() {
     <div>
       {/* The searchbar is handled in its own component as it will be re-used */}
         <form className="d-flex search">
-            <input className="form-control me-2" type="search" placeholder="Search..." aria-label="Search" />
+            <input type="search" placeholder="Search..." aria-label="Search" />
             <i className="fa fa-search icon" aria-hidden="true"></i>
         </form>
     </div>
