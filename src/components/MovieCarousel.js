3a092d2d5c79449b385a8255137ed765f238a33e
Caleb <kamenjuhkiiru@gmail.com>
2023-11-09 12:36:13 +0300

    Carousel swiper

---
+++ src/components/MovieCarousel.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../css/movieCarousel.css'
+
+// We need to npm install the swiper
+// import its files from there
+
+function MovieCarousel() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default MovieCarousel
