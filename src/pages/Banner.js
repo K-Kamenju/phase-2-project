1226638206c3a3e6cc446048a4f6858a973dfaa9
Caleb <kamenjuhkiiru@gmail.com>
2023-11-09 11:24:40 +0300

    Movie banner and content

--- src/pages/Banner.js
+++ src/pages/Banner.js
@@ -67,24 +67,27 @@ function Banner() {
             <img src={bgImage} alt='Background' className='img-fluid bgImg active' />
             <div className='container-fluid'>
                 <div className='row'>
-                    <div className='col-lg-6 col-md-12'>
-                        <div className='content'>
+                    <div className='col-md-6'>
+                        <div className='content active'>
                             <h2 className="heading" id="h2Element">Click the Movie Cards</h2>
                             <h4 id="h4Element">
                                 <span className="release-year" id="release">2023</span>
                                 <span className="rating" ><i className="fa fa-star" aria-hidden="true" id="rating"> 7.5</i></span>
                             </h4>
-                            <p className="overview" id="p-overview">
+                            <p className="overview">
                                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum fuga sit excepturi placeat iste nisi! Et quibusdam dolorum iure quasi nemo quaerat omnis incidunt beatae quisquam sint, odit sunt cum, impedit aliquid eius architecto soluta. Ut nobis voluptatum vitae adipisci laborum aspernatur! Deserunt magni autem quisquam impedit dolor quae inventore.
                             </p>
                             <div className="button">
                                 <a href="#" className="watch"><i className="fa fa-play watch-trailer" aria-hidden="true"> Watch Trailer </i></a>
-                                <a href="#" className="my-comment"><i className="fa fa-comment" aria-hidden="true" id="interact"></i></a> 
+                                <a href="#" className="my-comment"><i className="fa fa-comment" aria-hidden="true" id="interact"> Blog</i></a> 
                             </div>
                         </div>
                     </div>
-                    <div className='col-lg-6 col-md-12'>
-                        <h2>one</h2>
+                    <div className='col-md-6'>
+                        <div className='trailer active'>
+                            <h2>one</h2>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
