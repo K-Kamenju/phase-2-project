4dc58f3b051780b94e9345e487a7a80e7ebec111
Caleb <kamenjuhkiiru@gmail.com>
2023-11-12 23:21:14 +0300

    Navbar collapse functionality

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,9 +10,9 @@ function NavBar() {
     // Navbar directly imported from bootstrap
     <nav className="navbar navbar-expand-lg nav">
         <div className="container-fluid">
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-            </button>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+   <span className="navbar-toggler-icon"></span>
+</button>
             <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                 <li className="nav-item">
